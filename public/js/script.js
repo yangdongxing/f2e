3418fcb52b9245e961ec555a8bb9460538d1390e
见见 <jyu213@gmail.com>
2016-03-14 18:05:41 +0800

    rebuilding site 2016年 3月14日 星期一 18时05分41秒 CST

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+$(function(){
+	$('.nav button.menu').click(function(){
+		$('.nav').toggleClass('expand');
+	});
+
+	$('.nav li,.assort li').click(function(){
+		window.location = $(this).find('a').attr('href');
+	});
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 500 && window.innerWidth > 960){
+			$("#gotop").fadeIn(500);
+		}else{
+			$("#gotop").fadeOut(500);
+		}
+	});
+
+	$('#gotop').click(function(){
+		$('body,html').animate({scrollTop:0},1000);
+	});
+});
