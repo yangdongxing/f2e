ce54665a10327590c7f9f23af8e514b806663de1
jianjian <xucj@dxyer.com>
2016-03-29 19:53:00 +0800

    rebuilding site 2016年 03月 29日 星期二 19:52:59 CST

--- hook/publish.js
+++ hook/publish.js
@@ -58,6 +58,9 @@ handler.on('issues', function (event) {
 */
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
