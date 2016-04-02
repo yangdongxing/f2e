b313bd896ec28262241ba0e06fcf2ceaba9322f2
见见 <jyu213@gmail.com>
2016-04-02 12:46:20 +0800

    hack: image url

--- static/js/script.js
+++ static/js/script.js
@@ -18,4 +18,17 @@ $(function(){
 	$('#gotop').click(function(){
 		$('body,html').animate({scrollTop:0},1000);
 	});
+
+	/**
+	 * Hack content static URL
+	 */
+	var $hackUrl = $('.J-hack-url');
+	if ($hackUrl.length) {
+		var baseURL = $hackUrl.data('url');
+		$('.J-hack-url img').each(function() {
+			var src = $(this).attr('src');
+			var newSrc = src.replace(/^\.\.\//, baseURL);
+			$(this).attr('src', newSrc);
+		});
+	}
 });
