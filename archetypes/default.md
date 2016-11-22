fcd878ec2d59155064cf6f7f025f865417c36402
huoteng <huot@dxy.cn>
2016-11-22 17:51:11 +0800

    rebuilding site Tue Nov 22 17:51:11 CST 2016

--- archetypes/default.md
+++
@@ -1,6 +0,0 @@
-+++
-title = "Markdown Demo"
-date = "2016-02-22T17:18:13+08:00"
-tags = ["Demo"]
-author = "f2e"
-+++
--- bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nohup hugo server -b http://f2e.dxy.net/blog --appendPort=false --buildDrafts --watch &
-
-nohup node hook/publish.js &
