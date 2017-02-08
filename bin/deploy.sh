779fbfb41b881d5edb282eeaaf04557683449c51
huoteng <huot@dxy.cn>
2017-02-08 11:59:23 +0800

    test

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -20,15 +20,5 @@ hugo
 git add -A
 git commit -m "$msg"
 
-# test
-
-
-
-# Push source and build repos.
-git push origin master -f
-# Push the public subtree to the gh-pages branch
-# git subtree push --prefix=public git@github.com:dxy-developer/f2e.git gh-pages
-
-# 貌似搞得太复杂了，不会解决冲突问题.. 直接删除从来
-git push origin --delete gh-pages
-git subtree push --prefix=public git@github.com:dxy-developer/f2e.git gh-pages
+# push to github
+git push git@github.com:dxy-developer/f2e.git master
---
+++ content/post/huot/test.md
@@ -0,0 +1,10 @@
++++
+title = "test"
+date = "2018-12-27"
+tags = ["test"]
+draft = false
+author = "huot"
+avatar = "huot.jpg"
++++
+
+# test
